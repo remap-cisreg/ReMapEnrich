8b3c631e502a88085d4ec81fec7a9dd8fd135ba1
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-02-28 15:17:54 +0100

    First Package Hello World

---
+++ R/hello_world.R
@@ -0,0 +1,7 @@
+#' @title Hello World
+#' @name hello_world
+#' @export
+hello_world = function()
+{
+    print("Hello world!")
+}
--- openEnrich.R
+++
@@ -1,5 +0,0 @@
-openEnrich = function(path)
-{
-    enrich = read.table(path, header = TRUE, skip = 1)
-    return(enrich)
-}
