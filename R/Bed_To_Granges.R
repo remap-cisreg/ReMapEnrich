62071135c2e63139cf5b1506fd74143b2e6e8e2c
Martin Mectdagh <mm8docksy421@hotmail.fr>
2017-03-06 10:30:09 +0100

    script format bed to Granges commit du vendredi raté

--- R/Bed_To_Granges.R
+++ R/Bed_To_Granges.R
@@ -14,18 +14,18 @@ bed_to_granges = function(file)
 {
     df = read.table(file, header=F, stringsAsFactors=F)
     if(length(df) > 6)
-    {
-        df <- df[,-c(7:length(df))]
+    {   
+        df = df[,-c(7:length(df))]
     }
     if(length(df)<3)
-    {
+    {   
         stop("File has less than 3 columns")
     }
     header = c('chr','start','end','id','score','strand')
     names(df) = header[1:length(names(df))]
     if('strand' %in% colnames(df))
     {
-        df$strand <- gsub(pattern="[^+-]+", replacement = '*', x = df$strand)
+        df$strand = gsub(pattern= "[^+-]+", replacement = '*', x = df$strand)
     }
     
     
@@ -36,7 +36,7 @@ bed_to_granges = function(file)
     } else 
         if (length(df)==4)
         {
-        gr <- with(df, GRanges(chr, IRanges(start, end), id=id))
+        gr = with(df, GRanges(chr, IRanges(start, end), id=id))
         } else 
             if (length(df)==5)
             {
