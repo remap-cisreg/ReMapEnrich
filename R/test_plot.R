d1c275cffcd9dc576defa5813367f3471b07ad03
Martin Mestdagh <martin.mestdagh@gmail.com>
2017-03-09 17:50:59 +0100

    oubli d'une ligne de code colfunc+ ajout du plot dans le test

--- R/test_plot.R
+++ R/test_plot.R
@@ -2,10 +2,11 @@
 #res=category.enrichment.bed("inst/extdata/mini_regions.bed","inst/extdata/mini_catalog.bed","inst/extdata/mini_genome.genome")
 
 
-res2=res[,5]
-names(res2) = res[,1]
-res2=sort(res2)
-output.plot=barplot(res2,horiz=TRUE,beside=TRUE,xlab="significance",ylab="category",space=0.5,width=0.5,col=colfunc(10))
+#res2=res[,5]
+#names(res2) = res[,1]
+#res2=sort(res2)
+#colfunc<-colorRampPalette(c("royalblue","red"))
+#output.plot=barplot(res2,horiz=TRUE,beside=TRUE,xlab="significance",ylab="category",space=0.5,width=0.5,col=colfunc(10))
 
 #png("plot.png")
 #dev.off()
--- tests/gr_enrichment_test.R
+++ tests/gr_enrichment_test.R
@@ -1,3 +1,11 @@
 cat = BedToGranges("big_data/nrPeaks_all.bed")
 query = BedToGranges("big_data/ENCFF001VCU.bed")
 enrich = GrEnrichment(query, cat)
+res2=enrich[,5]
+names(res2) = enrich[,1]
+res2=sort(res2)
+res2=res2[(length(res2)-10):length(res2)]
+colfunc<-colorRampPalette(c("royalblue","red"))
+output.plot=barplot(res2,horiz=TRUE,beside=TRUE,xlab="significance(log10(Pval))",space=0.5,width=0.5,
+                    cex.names=0.8,col=colfunc(length(res2)),las=2)
+
