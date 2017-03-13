0b9946aa8a588e3e0ffe438539b453b9b912ffc8
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-03-13 12:33:09 +0100

    Commentaire sur les fonctions utils

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,25 @@
+#' Get chromosome file
+#'
+#' Get preloaded chromosome sizes files.
+#' 
+#' @param genome The name of the species to import the chromosomes from.
+#' 
+#' @return The path to a file that contains the chromosome lengths.
+#' 
 #' @export
 GetChromFile <- function(genome)
 {
     return(system.file("extdata", paste(genome, ".genome", sep = ""), package = "roken"))
 }
 
+#' Get chromosome sizes
+#'
+#' Get preloaded chromosome sizes file into a table.
+#' 
+#' @param genome The name of the species to import the chromosomes from.
+#' 
+#' @return A table that contains the chromosome lengths.
+#' 
 #' @export
 GetChromSizes <- function(genome)
 {
