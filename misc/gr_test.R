0de5d92f788b65e9187ca2f298df9393094c625d
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-03-14 15:37:57 +0100

    test locaux

--- misc/gr_test.R
+++ misc/gr_test.R
@@ -2,15 +2,4 @@ queryFile = "inst/extdata/ReMap_nrPeaks_public_chr22_SOX2.bed"
 catFile = "inst/extdata/ReMap_nrPeaks_public_chr22.bed"
 query = BedToGranges(queryFile)
 cat = BedToGranges(catFile)
-c2 = rep(0,20)
-for(i in 1:20){
-    print(i)
-    for(j in 1:5){
-        cat("",j,"|")
-        enrichment = GrEnrichment(query, cat, lower = FALSE)
-        enrichment2 = GrEnrichment(query, cat, lower = FALSE)   
-        l = enrichment$category == enrichment2$category
-        c2[i] = c2[i] + match(FALSE, l)
-    }
-    cat("\n")
-}
+enrichment = GrEnrichment(query, cat, lower = FALSE)
