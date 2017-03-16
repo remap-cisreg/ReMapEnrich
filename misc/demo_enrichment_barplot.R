6b6cab4f5ca57f32fcfd96dd4f6a8092cf57108f
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2017-03-16 15:33:52 +0100

    set queryFile and catfile as options

--- misc/demo_enrichment_barplot.R
+++ misc/demo_enrichment_barplot.R
@@ -1,13 +1,18 @@
 # Démonstration de l'exemple 1 de Annotbed avec deux plots en sorti: les 10 premiers peaks et 20 premiers
 
-demo_time <- function()
-{   queryFile <- "big_data/ENCFF001VCU.bed"
-    catFile <- "big_data/nrPeaks_all.bed"
+demo_time <- function(queryFile = "big_data/ENCFF001VCU.bed",
+                      catFile = "big_data/nrPeaks_all.bed") {
     query <- BedToGranges(queryFile)
     cat <- BedToGranges(catFile)
     enrich <- GrEnrichment(query, cat, shuffles = 10, lower = FALSE)
     par(mfrow = c(1, 2))
-    EnrichmentBarPlot(enrich,lengthData = 10)
-    EnrichmentVolcanoPlot(enrich)
+    EnrichmentBarPlot(enrich,lengthData = 20)
+    EnrichmentVolcanoPlot(enrich,aRisk = 0.05)
+    return(enrich)
 }
-print(system.time(demo_time()))
+
+print(system.time(enrich <- demo_time()))
+
+
+
+print(system.time(enrich <- demo_time(queryFile = "big_data/SOX2_peaks.bed")))
