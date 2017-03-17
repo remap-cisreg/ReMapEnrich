296538655fa35a684be70fb4709381d5707c4854
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-03-17 09:10:42 +0100

    Resolution des conflits

--- R/export_enrichment.R
+++ R/export_enrichment.R
@@ -6,28 +6,10 @@
 #'  @param plot Boolean option T or F if user wants a plot. 
 #'      
 #'  @export
-ExportEnrichment  <- function(dat, fileName = "output", format = "tsv")
-{
-    if(format == "csv")
-    {
+ExportEnrichment  <- function(dat, fileName = "output", format = "tsv") {
+    if(format == "csv") {
         write.csv(dat, file = fileName)
-    }
-    if (format == "tsv")
-    {
+    } else {
         write.table(dat, file = fileName, quote = FALSE, sep = '\t', col.names = NA)
     }
-<<<<<<< HEAD:R/export_enrichment.R
-=======
-    else
-    {
-        # Realize a capture of the dataframe if format isn't selected.
-        capture.output(dat, file = fileName)
-    }
-    # Verify if user wants a plot with the output file.
-    if(plot == TRUE)
-   {
-        # Create a default plot = barplot.
-        EnrichmentBarPlot(dat, lengthData = 10, aRisk = 0.05)
-   }
->>>>>>> 38ed497669a3ef74e6805fe2730e79178733726b:R/export.R
 }
