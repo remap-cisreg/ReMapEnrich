80206f927bfeb21016ea8633f34c6dbcf1626ca7
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-03-17 13:38:50 +0100

    Test random

--- misc/random_test.R
+++ misc/random_test.R
@@ -1,16 +1,10 @@
-# Feed the matrix
+query = BedToGranges("big_data/ENCFF001VCU.bed")
 catalog = BedToGranges("big_data/nrPeaks_all.bed")
-categories = unique(catalog@elementMetadata$id)
-#
-megaMat = array(dim = c(length(categories), 50, 50))
-sizeRanges = seq(1000,50000, 1000)
-nbRanges = seq(1000, 50000, 1000)
-for(cat in 1:length(categories)){
-    for(size in 1:length(sizeRanges)){
-        for(nb in 1:length(nbRanges)){
-            print(nbRanges[nb])
-            print(sizeRanges[size])
-            megaMat[cat, size, nb] = GrIntersect(GenRegions(nbRanges[nb], sizeRanges[size]), catalog)[categories[cat]]
-        }
-    }
+pVals = c()
+lambdas = c()
+for(i in 1:2) {
+    cat(i, "\r")
+    enrichment = GrEnrichment(GrShuffle(query), catalog)
+    pVals = c(pVals, enrichment$adjusted.p.value)
+    lambdas = c(lambdas, enrichment$random.average)
 }
