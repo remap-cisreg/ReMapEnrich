4161356dba362b93cb9dbed1ff8bc5f87e208c31
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-03-21 13:02:21 +0100

    load ucsc + random test

--- R/load_ucsc_chrom_sizes.R
+++ R/load_ucsc_chrom_sizes.R
@@ -5,19 +5,22 @@
 #' 
 #' @param id The ucsc id of the assembly.
 #' @param file="" The path to a file where the chromosomes sizes will be saved. If empty no file is created.
+#' @param all=FALSE If TRUE then all the chromosomes sizes will be returned in the data frame, only the ones with no "_" in their names will be returned.
 #' 
-#' @return A table that contains the chromosome lengths.
+#' @return A vector that contains the chromosome lengths.
 #' 
 #' @export
-LoadUcscChromSizes <- function(id, file = "") {
+LoadUcscChromSizes <- function(id, file = "", all = FALSE) {
     path <- file
     if(file == "")
         path <- tempfile()
     command <- paste("mysql  --user=genome --host=genome-mysql.cse.ucsc.edu -A -D", id, "-e 'select chrom,size from chromInfo' >", path)
     system(command)
-    if(file == "")
+    if(file != "")
         return(path)
     chromSizes <- read.table(path, header = TRUE, sep = "\t",stringsAsFactors = FALSE, quote = "", row.names = 1)
-    unlink(tempFile)
+    unlink(path)
+    if(!all)
+        chromSizes <- chromSizes[-grepl("_", rownames(chromSizes))]
     return(chromSizes)
 }
--- misc/random_test.R
+++ misc/random_test.R
@@ -1,10 +1,18 @@
-query = BedToGranges("big_data/ENCFF001VCU.bed")
-catalog = BedToGranges("big_data/nrPeaks_all.bed")
+catalog = BedToGranges("big_data/wgEncodeRegTfbsClusteredV3.bed")
 pVals = c()
 lambdas = c()
-for(i in 1:2) {
+nbIntersects = c()
+effectSizes = c()
+for(i in 1:10) {
     cat(i, "\r")
-    enrichment = GrEnrichment(GrShuffle(query), catalog)
+    enrichment = GrEnrichment(GenRegions(1000,3000), catalog)
+    enrichment = enrichment[order(enrichment$category),]
     pVals = c(pVals, enrichment$adjusted.p.value)
     lambdas = c(lambdas, enrichment$random.average)
+    nbIntersects = c(nbIntersects, enrichment$nb.overlaps)
+    effectSizes = c(effectSizes, enrichment$effect.size)
 }
+effectSizes = matrix(effectSizes, ncol = 10)
+pVals = matrix(pVals, ncol = 10)
+nbIntersects = matrix(nbIntersects, ncol = 10)
+lambdas = matrix(lambdas, ncol = 10)
