681262a3f64600d40d9e5a5367ade469436165cc
Martin Mestdagh <martin.mestdagh@gmail.com>
2017-03-21 16:59:45 +0100

    Modification for the coloration of plot in progress

--- R/enrichment_plot.R
+++ R/enrichment_plot.R
@@ -7,13 +7,13 @@
 #'  @param aRisk=0.05 The alpha risk, by default 0.05.
 #'  @param sigDisplayQuantile=0.95 quantile used to define the maximal value for the
 #'  Y axis, based on a quantile.
-#'  @param col="Dark2" Allows you to use colorBrewer palettes.
+#'  @param coloration="Accent" Allows you to use colorBrewer palettes.
 #'  @export
 EnrichmentBarPlot <- function(enrich, 
                               lengthData = 20,
+                              sigDisplayQuantile = 0.95,
                               aRisk = 0.05,
-                              col = "Dark2") {
-    
+                              coloration = "Accent") {
     adjustedSignificance <- enrich$adjusted.significance
     names(adjustedSignificance) <- enrich$category
     sortedAdjustedSignificance <- sort(adjustedSignificance)
@@ -21,9 +21,11 @@ EnrichmentBarPlot <- function(enrich,
         sortedAdjustedSignificance[(length(sortedAdjustedSignificance)-lengthData):length(sortedAdjustedSignificance)]
 
     # Create a gradient stain.
-    colorFunction <- c(colorRampPalette(brewer.pal(8, col))(length(sortedAdjustedSignificance)))
+    colorFunction <- c(colorRampPalette(brewer.pal(8, coloration))(length(sortedAdjustedSignificance)))
+
     # Give a title for the barplot with lengthData.
     titlePlot = c("Significance of first", lengthData, "category")
+    
     # Create barplot with legend.
     barplot(sortedAdjustedSignificance,
             main      = titlePlot,
@@ -33,7 +35,7 @@ EnrichmentBarPlot <- function(enrich,
             beside    = TRUE, 
             space     = 0.5,
             width     = 0.5,
-            cex.names = 0.8,
+            cex.names = 1,
             las       = 2
             )
     
@@ -41,6 +43,7 @@ EnrichmentBarPlot <- function(enrich,
     aSignificance <- ( -10 * log10(aRisk ))
     if (!is.finite(aSignificance))
         stop("The alpha risk is too small to be computed.")
+    
     # Add a line that shows the alpha risk.
     abline(v = aSignificance, lty = 5)
     mtext(bquote(alpha == .(aSignificance)), side = 3, at = aSignificance, col = "red")
@@ -54,17 +57,16 @@ EnrichmentBarPlot <- function(enrich,
 #'  @param aRisk=0.05 The alpha risk, by default 0.05.
 #'  @param sigDisplayQuantile=0.95 quantile used to define the maximal value for the
 #'  Y axis, based on a quantile. 
-#'  @param col="Dark2" Allows you to use colorBrewer palettes.
+#'  @param coloration="Accent" Allows you to use colorBrewer palettes.
 #'
 #'  @export
 EnrichmentVolcanoPlot <- function(enrich,
                                   sigDisplayQuantile = 0.95,
                                   aRisk = 0.05,
-                                  col = "Dark2") {
+                                  coloration = "Accent") {
     # Create a gradient stain.
-    colorFunction <- c(colorRampPalette(brewer.pal(8, col))(length(enrich$adjusted.significance)))
-    effectsSize <- log(enrich$random.average/enrich$nb.overlaps,
-                        base = 2)
+    colorFunction <- c(colorRampPalette(brewer.pal(8, coloration))(length(enrich$adjusted.significance)))
+    
 
     y <- enrich$adjusted.significance
     yMax <- quantile(x = y, probs = sigDisplayQuantile)
@@ -75,19 +77,20 @@ EnrichmentVolcanoPlot <- function(enrich,
     pch[outsiders] <- 6
     
     # Create a volcanoplot-like.
-    plot(x    = effectsSize,
+    plot(x    = enrich$effect.size,
          y    = y,
          ylim=c(0,yMax),
          main = "Volcano plot",
          xlab = "Effect size",
          ylab = "Significance",
-         col  =  colorFunction,
+         col  =  c(1:15),
          pch  = pch,
-         cex  = 0.5)
+         cex  = 0.8)
     # Convert alpha risk from p-value to significance.
     aSignificance <- ( -10 * log10(aRisk))
     if (!is.finite(aSignificance))
         stop("The alpha risk is too small to be computed.")
+    
     # Add a line that shows the alpha risk.
     abline(v = aSignificance, h = 0, col = "red", lty = 5)
     mtext(bquote(alpha == .(aSignificance)), side = 4, at = aSignificance, col = "red")
@@ -102,7 +105,7 @@ EnrichmentVolcanoPlot <- function(enrich,
 #'  @param enrich The file enrichment from which the plot will be create.
 #'  @param lengthData The number of category for the plot.
 #'  @export
-EnrichmentPiePlot <- function(enrich, lengthData = 10)
+EnrichmentPiePlot <- function(enrich, lengthData = 10,coloration = "Reds")
 {
     dataPie <- enrich$nb.overlaps
     names(dataPie) <- enrich$category
@@ -115,6 +118,6 @@ EnrichmentPiePlot <- function(enrich, lengthData = 10)
     titlePie <- c("Percent of overlap of first", lengthData, "category")
     pie(dataPie,
         main   = titlePie,
-        col    = colorFunction(length(dataPie)),
+        col    = coloration,
         labels = labelPie)
 }
