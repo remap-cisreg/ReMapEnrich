66185eebbf7e49fcaadfc6e9652371b2c0887b1e
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2017-03-23 16:00:43 +0100

    added spacing. Still to be revised + add cum max

--- R/extract_enrichment.R
+++ R/extract_enrichment.R
@@ -11,30 +11,38 @@
 #' @return A data frame containing the enrichment informations.
 ExtractEnrichment <- function (categories, lower, catCount, theoricalMeans) {
     catNumber <-length(categories)
+
     # The p values are get with log transformation for computing extreme values.
     logPvals <- ppois(catCount, theoricalMeans, lower = lower, log = TRUE)
+
     # The vectors are sorted for the calculation of the q values.
     order <- order(logPvals)
     theoricalMeans <- theoricalMeans[order]
     catCount <- catCount[order]
     logPvals <- sort(logPvals)
     categories <- names(logPvals)
+    
     # Different logarithmic values are calculated for the q value.
     logN <- log(catNumber)
     logI <- log(1:catNumber)
-    logC <- log(sum(1/1:catNumber))
+    logC <- log(sum(1/(1:catNumber)))
+
     # This is the logarithm of the q values.
     logQVals <- ((logPvals + logN) - logI) + logC
+    
     # This is the logarithm of the e values.
     logEVals <- logPvals + log(catNumber)
+    
     # The different significances are computed from the logarithmic p and q values.
     sigPVals <- - (logPvals / log(10))
     sigQVals <- - (logQVals / log(10))
     sigEVals <- - (logEVals / log(10))
+    
     # The p e and q values are retrieved from the corresponding logarithmic values.
     pVals <- exp(logPvals)
     qVals <- exp(logQVals)
     eVals <- exp(logEVals)
+    
     #The enrichment informations don't make any sense for theorical means at 0.
     sigPVals[theoricalMeans == 0] = NA
     pVals[theoricalMeans == 0] = NA
@@ -42,6 +50,7 @@ ExtractEnrichment <- function (categories, lower, catCount, theoricalMeans) {
     qVals[theoricalMeans == 0] = NA
     sigEVals[theoricalMeans == 0] = NA
     eVals[theoricalMeans == 0] = NA
+
     # Computation of the effecct size.
     effectSizes <- log(catCount / theoricalMeans, base = 2)
     # Creation of the data frame with all the enrichment informations.
