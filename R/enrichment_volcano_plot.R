c67e86cf8bc9f748cf8363a3c1df6425d06c9e67
Martin Mestdagh <martin.mestdagh@gmail.com>
2017-03-31 13:20:47 +0200

    order of variables

--- R/enrichment_volcano_plot.R
+++ R/enrichment_volcano_plot.R
@@ -21,7 +21,7 @@ EnrichmentVolcanoPlot <-function(enrich,
                                  main = "Volcano plot",
                                  aRisk = 0.05,
                                  sigDisplayQuantile = 0.95,
-                                 col = c("#6699ff", "#ff5050"),
+                                 col = c("#ff5050", "#6699ff"),
                                  sigType = "q",
                                  ylim = c(0,yMax),
                                  xlab = "Effect size",
@@ -30,45 +30,46 @@ EnrichmentVolcanoPlot <-function(enrich,
                                  cex = 0.8,
                                  las = 1,
                                  ...) {
+   
     
+    sigTypeTitle <- paste(sigType,"-significance",sep = "")     
     sigType = paste(sigType,".significance",sep = "")
-    sigTypeTitle <- paste(sigType,"-significance",sep = "")
     
+   
+   # Sort the dataframe by q.significance decreasing.
+   enrich <- enrich[order(enrich[,sigType], decreasing = TRUE),]
+   enrich$category <- factor(enrich$category, 
+                             levels = enrich$category[order(enrich[,sigType])])
+   # Create a gradient stain.
+   colorFunction <- c(colorRampPalette(col)(length(enrich$category)))
     
-    # Sort the dataframe by q.significance decreasing.
-    enrich <- enrich[order(enrich[,sigType], decreasing = TRUE),]
-    enrich$category <- factor(enrich$category, 
-                              levels = enrich$category[order(enrich[,sigType])])
-    # Create a gradient stain.
-    colorFunction <- c(colorRampPalette(col)(length(enrich$category)))
+   #Create the ymax with sigdisplayQuantile.
+   y <- enrich[,sigType]
+   yMax <- quantile(x = y, probs = sigDisplayQuantile)
+   outsiders <- y > yMax
+   y[outsiders] <- yMax 
     
-    #Create the ymax with sigdisplayQuantile.
-    y <- enrich[,sigType]
-    yMax <- quantile(x = y, probs = sigDisplayQuantile)
-    outsiders <- y > yMax
-    y[outsiders] <- yMax 
+   # Transform point outside quantile with triangle.
+   pch <- rep(x = 19, length.out=nrow(enrich))
+   pch[outsiders] <- 17
+   
+   plot(x    = enrich$effect.size,
+        y    = y,
+        ylim = ylim,
+        main = main,
+        xlab = xlab,
+        ylab = ylab,
+        col  = colorFunction,
+        pch  = pch,
+        cex  = cex,
+        las  = las,
+        ...)
+   
+   # Calculate the new alpha risk.
+   sigAlpha <- -log10(aRisk)
+   sigAlpha <- round(sigAlpha, 3)
     
-    # Transform point outside quantile with triangle.
-    pch <- rep(x = 19, length.out=nrow(enrich))
-    pch[outsiders] <- 17
-    
-    plot(x    = enrich$effect.size,
-         y    = y,
-         ylim = ylim,
-         main = main,
-         xlab = xlab,
-         ylab = ylab,
-         col  = colorFunction,
-         pch  = pch,
-         cex  = cex,
-         las  = las,
-         ...)
-    
-    # Calculate the new alpha risk.
-    sigAlpha <- -log10(aRisk)
-    sigAlpha <- round(sigAlpha, 3)
-    
-    # Add a line that shows the alpha risk.
-    abline(h = sigAlpha, lty = 5, col = "Red")
-    mtext(bquote(alpha == .(sigAlpha)), side = 4, at = sigAlpha,las = 3, col = "Red")
+   # Add a line that shows the alpha risk.
+   abline(h = sigAlpha, lty = 5, col = "Red")
+   mtext(bquote(alpha == .(sigAlpha)), side = 4, at = sigAlpha,las = 3, col = "Red")
 }
