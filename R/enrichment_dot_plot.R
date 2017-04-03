9aae47410f63ab49821757b6ddd2bfeed86313aa
Martin Mestdagh <martin.mestdagh@gmail.com>
2017-04-03 14:39:03 +0200

    Descirption, comments and correction

--- R/enrichment_dot_plot.R
+++ R/enrichment_dot_plot.R
@@ -3,101 +3,128 @@
 #' @description Creates a dotplot from the enrichment(category=f(q.significance))
 #'  
 #' @param enrich The enrichment data frame from which the plot will be created.
-#' @param top=10 The number of category for the plot.
-#' @param main=c("Significance of first", top, "category"). Allows to choose the title of the plot.
-#' @param aRisk=0.05 The alpha risk, by default 0.05.
-#' @param col=c("#6699ff","#ff5050") Palette of coloration for the histogram 
-#'  with personnal color or RColorBrewer palette.
-#' 
+#' @param op=20 The number of category for the plot.
+#' @param main=paste("Significance, top", top, "categories"). Allows to choose the title of the plot.
+#' @param aRisk=0.05 The alpha risk.
+#' @param col=c("#6699ff","#ff5050") Palette of coloration for the plot 
+#' Personnal coloration such as c("#FEE0D2","#FC9272") or a RColorBrewer such as brewer.pal(5,"Reds").
+#' @param minCircleSize=0.5 Allows to change the minimum size of circle for the plot.
+#' @param inches=1/4 Modify the area of points.
+#' @param xlab="Mapped peaks ratio" Allows to change the title of x-axis.
+#' @param stainCol=rgb(colorFunction(((sig - minSigLim) / (maxSigLim - minSigLim))), maxColorValue = 255)
+#' Changes the color of perimeter of each point, by défault the coloration for the plot.#' 
 #' 
 #' @examples
 #' EnrichmentDotPlot(enrich = enrich)
 #' @export
 EnrichmentDotPlot <- function(enrich, 
-                              top = 10,
+                              top = 20,
                               main = paste("Significance, top", top, "categories"),
                               sigType = "q",
-                              col = c("#ff5050","#6699ff"),
-                              staincol = NULL) {
+                              col = c("#6699ff", "#ff5050"),
+                              minCircleSize = 0.5,
+                              inches = 1 / 4,
+                              xlab    = "Mapped peaks ratio",
+                              stainCol = rgb(colorFunction(((sig - minSigLim) / (maxSigLim - minSigLim))), maxColorValue = 255),
+                              ...) {
     
-    
+    # Create the grid for the plot and his legend.
     defPar <- par(no.readonly = TRUE)
     layout(matrix(c(1,1,2,3),2,2), widths = c(0.8,0.2))
+    
     # Allows to take the significance chosen.
-    sigTypeTitle <- paste(sigType,"-significance",sep = "")
-    sigType <- paste(sigType,".significance",sep = "")
+    sigTypeTitle <- paste(toupper(sigType), "-significance", sep = "")
+    sigType <- paste(sigType, ".significance", sep = "")
     
     
-    # Transform the dataframe which the length chosen by user.
-    enrich <- enrich [order(enrich[,sigType]),]
+    # Order the dataframe by significance chosen.
+    enrich <- enrich [order(enrich[,sigType], decreasing = TRUE),]
     
-    # Create a matrix to take the names of category.
+    # Modify the dataframe with the length chosen and order it.
     enrich <- enrich[1:top,]
+    enrich <- enrich [order(enrich[,sigType], decreasing = FALSE),]
+   
+    # Calculate the minimum and maximum of signifiance to create the legend.
     sig <- enrich[,sigType]
-    names(sig) <- enrich$category
-    
-    
-    minSig <- round(min(sig))
-    maxSig <- round(max(sig))
+    minSig <- floor(min(sig))
+    maxSig <- ceiling(max(sig))
     stepSig <- 10 ** (floor(log10(maxSig)) - 1)
     maxSigLim <- maxSig - (maxSig %% stepSig) + stepSig
     minSigLim <- minSig - (minSig %% stepSig)
     
+    # Calculate the minimum and maximum of overlaps to create the legend.
+    overlaps <- enrich$nb.overlaps
+    minOverlaps <- floor(min(overlaps))
+    maxOverlaps <- ceiling(max(overlaps))
+    stepOverlaps <- 10 ** (floor(log10(maxOverlaps)) - 1)
+    maxOverlapsLim <- maxOverlaps - (maxOverlaps %% stepOverlaps) + stepOverlaps
+    minOverlapsLim <- minOverlaps - (minOverlaps %% stepOverlaps)
+    
+    
     # Create the coloring palette
-    # (Personnal coloration such as c("#FEE0D2","#FC9272") or a RColorBrewer such as brewer.pal(5,"Reds").
-
+    # (Personnal coloration such as c("#FEE0D2", "#FC9272") or a RColorBrewer such as brewer.pal(5,"Reds").
     colorFunction <- colorRamp(col)
 
     # Creation of the dot plot.
-    with(enrich, symbols(x       = mapped.peaks.ratio,
-                                        y       = 1:top,
-                                        circles = nb.overlaps,
-                                        inches  = 1/4,
-                                        bg      = rgb(colorFunction(1 - ((sig - minSigLim) / (maxSigLim - minSigLim))), maxColorValue = 255),
-                                        fg      = staincol,
-                                        main    = main,
-                                        xlab    = "Mapped peaks ratio",
-                                        ylab    = "",
-                                        las     = 1,
-                                        yaxt    ='n'))
-
-    # Create the axis with name's category.
-    axis(2, at =1:top, labels = names(sig), las = 1, cex.axis=0.8)
-
-    par(mar=c(0, 0, 4, 0))
-    # Legend of the plot.
-    colorMatrix <- colorRampPalette(col)
-    plot(c(0,2),c(minSigLim,maxSigLim),type = 'n', axes = FALSE, ylab = "", xlab = sigTypeTitle)
-    text(x = 0.5, y = seq(minSigLim,maxSigLim, l = 5), labels = round(seq(minSigLim,maxSigLim,l = 5)), adj = 0)
-    gradientLegend <- as.raster(matrix(colorMatrix(20)), ncol = 1)
-    rasterImage(gradientLegend, 0,minSigLim,0.3,maxSigLim)
+    par(mar=c(5, 5, 4, 0))
+    with(enrich,
+         symbols(
+            x       = mapped.peaks.ratio,
+            y       = 1:top,
+            circles = (nb.overlaps - minOverlaps) / (maxOverlapsLim - minOverlapsLim) + minCircleSize,
+            inches  = inches,
+            bg      = rgb(colorFunction(((sig - minSigLim) / (maxSigLim - minSigLim))), maxColorValue = 255),
+            fg      = stainCol,
+            main    = main,
+            xlab    = xlab,
+            ylab    = "",
+            las     = 1,
+            yaxt    ='n',
+            ...
+            )
+         )
+    cols = rgb(colorFunction( ((sig - minSigLim) / (maxSigLim - minSigLim))), maxColorValue = 255)
+    for (i in 1:top) {
+        abline(h = i, lty = 5, lwd = 0.5, col = cols[i])
+    }
     
-    par(mar=c(5, 0, 0, 0))
-    ## Circles
+    # Create the axis with name's category.
+    axis(2, at = 1:top, labels = enrich$category, las = 1, cex.axis = 0.8)
+
+    par(mar=c(0, 0, 7, 0))
+    # Legend of the plot. Gradient coloration by significance.
+    colorMatrix <- colorRampPalette(rev(col))
+    plot(c(0,1),c(minSigLim, maxSigLim),type = 'n', axes = FALSE, ylab = "", xlab = sigTypeTitle)
+    text(x = 0.5, y = seq(minSigLim, maxSigLim, l = 5), labels = round(seq(minSigLim, maxSigLim, l = 5)), adj = 0)
+    gradientLegend <- as.raster(matrix(colorMatrix(20)), ncol = 1)
+    rasterImage(gradientLegend, 0, minSigLim, 0.3, maxSigLim)
+    mtext(sigTypeTitle, 3, cex = 0.8)
+    
+    
+    par(mar=c(5, 0, 3, 0))
+    # Legend of the plot. Size of plot by number of overlaps.
     nCircles <- 5
-    #plot(c(0,1), c(0,1), type ='n',axes = FALSE, xlab = "", ylab = "" , main = "Nb Overlaps")
-    minOverlaps <- min(enrich$nb.overlaps)
-    maxOverlpas <- max(enrich$nb.overlaps)
-    sizes <- seq(minOverlaps,maxOverlpas, l = nCircles)
-    yCircles <- c(1:nCircles/nCircles)
-    circles <- data.frame(sizes,yCircles)
-    with (circles, symbols(
-        x = rep(0,nCircles),
-        y = yCircles,
-        circles = sizes,
-        inches  = 1/4,
-        ylab = "",
-        xlab = "",
-        bty = 'n',
-        xaxt = 'n',
-        yaxt = 'n',
-        fg = "black",
-        bg = "black",
-        xlim = c(-0.01,0.05)
+    sizes <- seq(minCircleSize, minCircleSize + 1, l = nCircles)
+    yCircles <- cumsum((sizes * inches) + minCircleSize ) 
+    circles <- data.frame(sizes, yCircles)
+    with(circles,
+         symbols(
+            x = rep(0,nCircles),
+            y = yCircles,
+            circles = sizes,
+            inches  = inches,
+            ylab = "",
+            xlab = "",
+            bty = 'n',
+            xaxt = 'n',
+            yaxt = 'n',
+            fg = "black",
+            bg = "black",
+            xlim = c( - 0.01, 0.05)
         )
     )
-    text(0,0.1,labels = "Number of overlaps")
-    
-    
+    text(x = 0.02, y = yCircles, labels = round(seq(minOverlapsLim, maxOverlapsLim, l = 5)), adj = 0)
+    mtext("Number of overlaps", 3, cex = 0.8)
+
     par(defPar)
 }
