11a4ba5ca7ba9eb88571cac1fe2bc151e397249d
Martin Mestdagh <martin.mestdagh@gmail.com>
2017-04-03 14:39:00 +0200

    Descirption, comments and correction

--- R/enrichment_bar_plot.R
+++ R/enrichment_bar_plot.R
@@ -3,18 +3,15 @@
 #' @description Creates a barplot from the enrichment.
 #'  
 #' @param enrich The enrichment data frame from which the plot will be created.
-#' @param lengthData=10 The number of category for the plot.
-#' @param main=c("Significance of first", lengthData, "category"). Allows to choose the title of the plot.
-#' @param aRisk=0.05 The alpha risk, by default 0.05.
-#' @param sigDisplayQuantile=0.95 quantile used to define the maximal value for the
+#' @param top=20 The number of category for the plot.
+#' @param main=paste("Significance, top", top, "categories") Allows to choose the title of the plot.
+#' @param aRisk=0.05 The alpha risk.
+#' @param sigDisplayQuantile=0.95 Quantile used to define the maximal value for the
 #' Y-axis, based on a quantile.
-#' @param col=c("#6699ff","#ff5050") Palette of coloration for the histogram 
-#'  with personnal color or RColorBrewer palette.
-#' @param sigType="q" Allows to choose between Q-significance or P-significance or E-significance.
-#' @param xlim=c(min(enrich[sigType]), max(enrich[sigType])).
-#' Change the length of x-axis.
-#' @param xlab="Significance" change the label of x-axis.
-#' @param horiz=TRUE Flip axis.
+#' @param col=c("#6699ff","#ff5050") Palette of coloration for the plot 
+#' Personnal coloration such as c("#FEE0D2","#FC9272") or a RColorBrewer such as brewer.pal(5,"Reds").
+#' @param sigType="q" Allows to choose between Q-significance, P-significance or E-significance.
+#' @param xlab=sigTypeTtitle Allows to change the title of x-axis.
 #' @param beside=TRUE Juxtaposing bar or not.
 #' @param space=0.2 Allows to change size bar.
 #' @param cex.names=1 Allows to change size of x-axis (flipped).
@@ -23,14 +20,13 @@
 #' 
 #' @export
 EnrichmentBarPlot <- function(enrich, 
-                              lengthData = 10,
-                              main = c("Significance of first", lengthData, "category"),
+                              top = 10,
+                              main = paste("Significance, top", top, "categories"),
                               aRisk = 0.05,
                               sigDisplayQuantile = 0.95,
-                              col = c("#6699ff","#ff5050"),
+                              col = c("#6699ff", "#ff5050"),
                               sigType = "q",
                               xlab = sigTypeTitle,
-                              horiz = TRUE, 
                               beside = TRUE, 
                               space = 0.1,
                               cex.names = 0.8,
@@ -39,45 +35,45 @@ EnrichmentBarPlot <- function(enrich,
                               ...) {
     
     # Creation of the two strings containing the right value to get from the enrichment.
-    sigTypeTitle <- paste(sigType,"-significance",sep = "")
-    sigType = paste(sigType,".significance",sep = "")
+    sigTypeTitle <- paste(toupper(sigType), "-significance", sep = "")
+    sigType = paste(sigType, ".significance", sep = "")
     
     # Creation of matrix with column adapted and with the length selected.
     sig <- vector()
     sig[enrich$category] <- enrich[,sigType]
     sig <- sort(sig)
-    sig <- sig[(length(sig)-lengthData):length(sig)]
+    sig <- sig[(length(sig) - top) : length(sig)]
 
     # Create the coloring palette
-    # (Personnal coloration such as c("#FEE0D2","#FC9272") or a RColorBrewer such as brewer.pal(5,"Reds").
-    colorFunction <- paste(colorRampPalette(col)(lengthData+1))
+    colorFunction <- paste(colorRampPalette(col)(top + 1))
 
-    #Create the xmax with sigdisplayQuantile.
+    #Create the xmax with sigDisplayQuantile.
     xMax <- quantile(x = sig, probs = sigDisplayQuantile)
     dispSig <- sig
     dispSig[sig > xMax] = xMax
 
     midPoints <- barplot(dispSig,
-            main      = main,
-            xlab      = xlab,
-            col       = colorFunction,
-            horiz     = horiz, 
-            beside    = beside, 
-            space     = space,
-            cex.names = cex.names,
-            border    = border,
-            las       = las,
-            xlim = c(0,xMax),
-            ...
-            )
-    labSig <- as.character(round(sig[sig>xMax]))
-    TextOutline(y = midPoints[sig>xMax], x = rep(xMax - (xMax*0.01),length(labSig)), adj = 1, labels = labSig, cex = 1.1)
+                         main      = main,
+                         xlab      = xlab,
+                         col       = colorFunction,
+                         horiz     = TRUE, 
+                         beside    = beside, 
+                         space     = space,
+                         cex.names = cex.names,
+                         border    = border,
+                         las       = las,
+                         xlim = c(0, xMax),
+                         ...
+                         )
+    labSig <- as.character(round(sig[sig >= xMax]))
+    TextOutline(y = midPoints[sig >= xMax], x = rep(xMax - (xMax*0.01), length(labSig)), 
+                adj = 1, labels = labSig, cex = 1.1)
     
     # Calculate the new alpha risk.
     sigAlpha <- -log10(aRisk)
     sigAlpha <- round(sigAlpha, 3)
     
     # Add a line that shows the new alpha risk.
-    abline(v = sigAlpha, lty = 5)
+    abline(v = sigAlpha, lty = 5, col = "red")
     mtext(bquote(alpha == .(sigAlpha)), side = 3, at = sigAlpha, col = "red")
 }
