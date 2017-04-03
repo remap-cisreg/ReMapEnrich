7ed12a669132f0ae155a8a4e9748e0abd0686800
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-04-03 14:29:40 +0200

    Added doc for data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' @title Enrichment example
+#' @author Zacharie Menetrier
+#' @description A dataframe containing an enrichment example.
+#' This enrichment has been made with SOX2 from chr22 as a query and all the remap catalogue of the chr22 as a catalogue.
+#' @format A data frame with 130 rows and 11 variables:
+#' @source
+#' queryFile <- system.file("extdata", "ReMap_nrPeaks_public_chr22_SOX2.bed", package = "roken")
+#' catFile <- system.file("extdata", "ReMap_nrPeaks_public_chr22.bed", package = "roken")
+#' query <- BedToGranges(queryFile)
+#' cat <- BedToGranges(catFile)
+#' enrichmentExample <- GrEnrichment(query, cat, shuffles = 200, lower = FALSE)
+"enrichmentExample"
