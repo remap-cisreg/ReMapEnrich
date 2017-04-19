8b316d539ed898ea1d68b0d91f8d054c2dd27985
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-04-19 12:38:58 +0200

    Correction

--- misc/adjust_test.R
+++ misc/adjust_test.R
@@ -1,6 +1,6 @@
 randomCatalog <- GenRegions(10000, 1000)
 randomCatalog@elementMetadata$id = "Vert"
-randomsCatalogIntersections <- RandomIntersections(catalog, iterations = 10000, regionNb = 1000, regionSize = 1000)
+randomsCatalogIntersections <- RandomIntersections(randomCatalog, iterations = 10000, regionNb = 1000, regionSize = 1000)
 chi2results <- AdjustToPoisson(randomsCatalogIntersections)
 h <- hist(randomsCatalogIntersections, breaks = 0:(max(randomsCatalogIntersections)+1), plot = FALSE)
 expOverlaps <- exp.overlaps <- dpois(x = 0:max(randomsCatalogIntersections), lambda = mean(randomsCatalogIntersections)) * sum(h$counts)
