959bc0bbc912f235bab3af6926a7e83a1fc95ecc
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2017-04-19 12:30:48 +0200

    Public testing

--- misc/adjust_test.R
+++ misc/adjust_test.R
@@ -1,8 +1,8 @@
-catalog <- GenRegions(10000, 1000)
-catalog@elementMetadata$id = "Vert"
-randoms <- RandomIntersections(catalog, iterations = 10000, regionNb = 1000, regionSize = 1000)
-chi2results <- AdjustToPoisson(randoms)
-h <- hist(randoms, breaks = 0:(max(randoms)+1), plot = FALSE)
-expOverlaps <- exp.overlaps <- dpois(x = 0:max(randoms), lambda = mean(randoms)) * sum(h$counts)
+randomCatalog <- GenRegions(10000, 1000)
+randomCatalog@elementMetadata$id = "Vert"
+randomsCatalogIntersections <- RandomIntersections(catalog, iterations = 10000, regionNb = 1000, regionSize = 1000)
+chi2results <- AdjustToPoisson(randomsCatalogIntersections)
+h <- hist(randomsCatalogIntersections, breaks = 0:(max(randomsCatalogIntersections)+1), plot = FALSE)
+expOverlaps <- exp.overlaps <- dpois(x = 0:max(randomsCatalogIntersections), lambda = mean(randomsCatalogIntersections)) * sum(h$counts)
 plot(h$counts, type = 'h', col = "red")
 lines(expOverlaps, col = "blue")
