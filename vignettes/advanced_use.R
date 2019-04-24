7e4e3074fbd749739c6a2de3e36751f898f21671
Benoit Ballester <4662116+benoitballester@users.noreply.github.com>
2019-04-24 22:10:08 +0200

    No BioC and update
    Rearranged some paragraphs, so we can follow the demo

--- vignettes/advanced_use.R
+++ vignettes/advanced_use.R
@@ -1,34 +1,42 @@
-## ---- echo = FALSE, message = FALSE----------------------------------------
+## ---- echo = FALSE, message = FALSE--------------------------------------
 knitr::opts_chunk$set(collapse = T, comment = "#>", eval=FALSE)
 options(tibble.print_min = 4L, tibble.print_max = 4L)
 library(dplyr)
 
-## --------------------------------------------------------------------------
+## ---- echo=TRUE----------------------------------------------------------
+#  # Create a local directory for the tutorial
+#  demo.dir <- "~/ReMapEnrich_demo"
+#  dir.create(demo.dir, showWarnings = FALSE, recursive = TRUE)
+#  
+#  # Use the function DowloadRemapCatalog
+#  remapCatalog2018hg38 <- downloadRemapCatalog(demo.dir)
+#  
+#  # Or download other versions
+#  remapCatalog2015hg19 <- downloadRemapCatalog(demo.dir, version = "2015", assembly = "hg19")
+
+## ---- echo=TRUE----------------------------------------------------------
+#  # Load the ReMap catalogue and convert it to Genomic Ranges
+#  remapCatalog <- bedToGranges(remapCatalog2018hg38)
+#  
+
+## ------------------------------------------------------------------------
 #  # Load the ReMapEnrich library
 #  library(ReMapEnrich)
 #  
-#  # Load the example catalogue BED file.
-#  
+#  # Load the example catalogue as a BED file.
 #  catalogFile <- system.file("extdata",
 #                                      "ReMap_nrPeaks_public_chr22.bed",
 #                                      package = "ReMapEnrich")
 #  catalog <- bedToGranges(catalogFile)
-#  
 
-## ---- echo=TRUE------------------------------------------------------------
-#  # Create a local directory for the tutorial
-#  demo.dir <- "~/ReMapEnrich_demo"
-#  dir.create(demo.dir, showWarnings = FALSE, recursive = TRUE)
-#  # Use the function DowloadRemapCatalog
-#  remapCatalog2018hg38 <- downloadRemapCatalog(demo.dir)
-#  # Downloading other versions
-#  remapCatalog2015hg19 <- downloadRemapCatalog(demo.dir, version = "2015", assembly = "hg19")
-
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 #  # Downloading the ENCFF001VCU regions.
 #  ENCFF001VCU <- bedToGranges(downloadEncodePeaks("ENCFF001VCU", demo.dir))
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
+#  enrichment <- enrichment(ENCFF001VCU, remapCatalog)
+
+## ------------------------------------------------------------------------
 #  # Download a universe.
 #  universe <- bedToGranges(downloadEncodePeaks("ENCFF718QVA", demo.dir))
 #  # Convert ReMap to GRanges
@@ -36,33 +44,53 @@ library(dplyr)
 #  # Create the enrichment with the universe.
 #  enrichment <- enrichment(ENCFF001VCU, remapCatalog, universe, nCores=2)
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 #  # Create the enrichment with a less restrictive universe.
 #  enrichment <- enrichment(ENCFF001VCU, remapCatalog, universe, included = 0.1, nCores=2)
 #  # 90% of the shuffled regions can now be outside of the universe regions.
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 #  # Create the enrichment with a less restrictive universe.
 #  enrichment <- enrichment(ENCFF001VCU, remapCatalog, universe, included = 0.1, byChrom = TRUE, nCores=2)
 #  # 90% of the shuffled regions can now be outside of the universe regions.
 #  # The shuffled regions are still in the same chromosome where they came from.
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 #  # Shuffling ENCFF001VCU
 #  shuffledENCFF001VCU <- shuffle(ENCFF001VCU, universe = universe, byChrom = TRUE)
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 #  # Generate 100 random regions with a size of 1000 bases pair.
 #  randomRegions <- genRegions(100, 1000)
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 #  hg38ChromSizes <- loadChromSizes("hg38")
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 #  # Example with rn5
 #  rn5ChromSizes <- downloadUcscChromSizes("rn5")
+#  
 #  # Creation of random regions in the rattus norvegicus genome.
 #  randomRegions <- genRegions(100, 1000, rn5ChromSizes)
+#  
 #  # Shuffling of regions in the rattus norvegicus genome.
 #  shuffledRegions <- shuffle(randomRegions, rn5ChromSizes)
+#  
+#  # Species relevant to current and future ReMap releases
+#  hg38ChromSizes <- downloadUcscChromSizes("hg38")
+#  hg19ChromSizes <- downloadUcscChromSizes("hg19")
+#  mm10ChromSizes <- downloadUcscChromSizes("mm10")
+#  dm6ChromSizes <- downloadUcscChromSizes("dm6")
+#  
+#  # Maybe one day
+#  ce11ChromSizes <- downloadUcscChromSizes("ce11")
+#  rn5ChromSizes <- downloadUcscChromSizes("rn5")
+#  
+#  #-- This warning message is known an issue from RMySQL
+#  # https://github.com/r-dbi/RMySQL/issues/37
+#  
+#  # Warning message:
+#  # In .local(conn, statement, ...) :
+#  #  Unsigned INTEGER in col 1 imported as numeric
+#  
 
