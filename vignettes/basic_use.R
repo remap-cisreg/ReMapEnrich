bd7a5b4308951bbfdeb6689ec1ddafc9e2d34def
Benoit Ballester <4662116+benoitballester@users.noreply.github.com>
2019-04-24 22:09:37 +0200

    Removed the BioC style
    Going with plain rmarkdown stule.
    Will switch tp github stylel soon.

--- vignettes/basic_use.R
+++ vignettes/basic_use.R
@@ -1,4 +1,4 @@
-## ---- echo=FALSE-----------------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------
 knitr::opts_chunk$set(collapse = T, 
                       comment = "##    ", 
                       eval=TRUE, 
@@ -9,7 +9,7 @@ knitr::opts_chunk$set(collapse = T,
 options(tibble.print_min = 4L, tibble.print_max = 4L)
 library(dplyr)
 
-## ----init_query, echo=TRUE-------------------------------------------------
+## ----init_query, echo=TRUE-----------------------------------------------
 # Load the ReMapEnrich library
 library(ReMapEnrich) 
 
@@ -22,7 +22,7 @@ catalog <- bedToGranges(system.file("extdata",
                                     "ReMap_nrPeaks_public_chr22.bed",
                                     package = "ReMapEnrich"))
 
-## ----echo=TRUE, results=TRUE-----------------------------------------------
+## ----echo=TRUE, results=TRUE---------------------------------------------
 # Check what the catalog contains
 print(catalog)
 
@@ -30,23 +30,23 @@ print(catalog)
 print(query)
 
 
-## ----echo=TRUE, results=TRUE-----------------------------------------------
+## ----echo=TRUE, results=TRUE---------------------------------------------
 enrichment.df <- enrichment(query, catalog, byChrom = TRUE)
 head(enrichment.df)
 
-## --------------------------------------------------------------------------
+## ------------------------------------------------------------------------
 # Display a bar plot
 enrichmentBarPlot(enrichment.df, sigDisplayQuantile = 0.5, top = 20, aRisk = 0.00001)
 
-## ---- echo=TRUE------------------------------------------------------------
+## ---- echo=TRUE----------------------------------------------------------
 # Display a volcano plot (na.omit() is mandatory as there is NAs in the enrichment data frame).
 enrichmentVolcanoPlot(na.omit(enrichment.df), sigDisplayQuantile = 0.9, aRisk = 0.00001)
 
-## ---- echo=TRUE, fig.height=7----------------------------------------------
+## ---- echo=TRUE, fig.height=7--------------------------------------------
 # Display a dot plot.
 enrichmentDotPlot(enrichment.df)
 
-## ---- echo=TRUE, fig.height=7----------------------------------------------
+## ---- echo=TRUE, fig.height=7--------------------------------------------
 # Display a dot plot without SOX2.
 enrichmentDotPlot(enrichment.df[enrichment.df$category != "SOX2",])
 
